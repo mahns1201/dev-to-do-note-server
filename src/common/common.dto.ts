ee139bab38455a7395ac14803c4eafbd7ae3ddd8
mahns <mahns1201@gmail.com>
2023-11-30 04:03:52 +0900

    FIX: Base Time, Response dto 설정

--- src/common/common.dto.ts
+++ src/common/common.dto.ts
@@ -1,21 +1,43 @@
 import { HttpStatus } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class SwaggerResponseDto<T> {
+export class BaseTimeDto {
   @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  deletedAt: Date;
+}
+
+export class BaseResponseDto {
+  @ApiProperty({ description: 'http 상태 코드' })
   httpStatus: HttpStatus;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'api 응답 메시지' })
   message: string;
-
-  @ApiProperty()
-  items?: T[];
-
-  @ApiProperty()
-  item?: T;
 }
 
 export interface ServiceResultDto<T> {
   items?: T;
+  item?: T;
+}
+
+/**
+ * @deprecated
+ */
+export class SwaggerResponseDto<T> {
+  @ApiProperty({ description: 'http 상태 코드' })
+  httpStatus: HttpStatus;
+
+  @ApiProperty({ description: 'api 응답 메시지' })
+  message: string;
+
+  @ApiProperty({ description: 'api 응답 결과가 복수개 일 때의 결과' })
+  items?: T[];
+
+  @ApiProperty({ description: 'api 응답 결과가 단수개 일 때의 결과' })
   item?: T;
 }
