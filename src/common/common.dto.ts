c15ac3886682e8d5b20f879d151565158c4206eb
mahns <mahns1201@gmail.com>
2023-12-02 19:48:23 +0900

    ADD: 페이징 dto 추가

--- src/common/common.dto.ts
+++ src/common/common.dto.ts
@@ -1,5 +1,6 @@
 import { HttpStatus } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
 
 export class BaseTimeDto {
   @ApiProperty()
@@ -23,6 +24,30 @@ export class BaseResponseDto {
 export class ErrorResponseDto extends BaseResponseDto {
   @ApiProperty({ description: 'error 메시지' })
   error: string;
+}
+
+export class PagingRequestDto {
+  @ApiProperty({ description: 'requestPage' })
+  @IsNumber()
+  page: number;
+
+  @ApiProperty({ description: 'limit' })
+  @IsNumber()
+  limit: number;
+}
+
+export class PagingResponseDto extends BaseResponseDto {
+  @ApiProperty({ description: 'currentPage' })
+  @IsNumber()
+  currentPage: number;
+
+  @ApiProperty({ description: 'limit' })
+  @IsNumber()
+  limit: number;
+
+  @ApiProperty({ description: 'totalCount' })
+  @IsNumber()
+  totalCount: number;
 }
 
 export interface ServiceResultDto<T> {
