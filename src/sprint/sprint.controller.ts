cc0ed7f954810716f327b85226345eb7fd9b9922
mahns <mahns1201@gmail.com>
2023-12-23 21:43:05 +0900

    ADD sprint-list 추가 및 페이징처리

--- src/sprint/sprint.controller.ts
+++ src/sprint/sprint.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import {
@@ -27,12 +29,30 @@ import {
 import { jwtUserT } from 'src/constant/jwt.constant';
 import { User } from 'src/decorator/user.decorator';
 import { RepoService } from 'src/repo/repo.service';
-import { ErrorResponseDto } from 'src/common/common.dto';
+import { ErrorResponseDto, PagingRequestDto } from 'src/common/common.dto';
+import { SprintDto } from './dto/sprint.dto';
+import { OutputFindSprintsDto } from './dto/find-sprint.dto';
 
 @Controller('sprint')
 @UseGuards(AuthGuard)
 @ApiTags('sprint')
 @ApiBearerAuth('accessToken')
+@ApiBadRequestResponse({
+  type: ErrorResponseDto,
+  status: HttpStatus.BAD_REQUEST,
+})
+@ApiUnauthorizedResponse({
+  type: ErrorResponseDto,
+  status: HttpStatus.UNAUTHORIZED,
+})
+@ApiNotFoundResponse({
+  type: ErrorResponseDto,
+  status: HttpStatus.NOT_FOUND,
+})
+@ApiInternalServerErrorResponse({
+  type: ErrorResponseDto,
+  status: HttpStatus.INTERNAL_SERVER_ERROR,
+})
 export class SprintController {
   constructor(
     private sprintService: SprintService,
@@ -46,18 +66,6 @@ export class SprintController {
   @ApiCreatedResponse({
     type: OutputCreateSprintDto,
     status: HttpStatus.CREATED,
-  })
-  @ApiBadRequestResponse({
-    type: ErrorResponseDto,
-    status: HttpStatus.BAD_REQUEST,
-  })
-  @ApiUnauthorizedResponse({
-    type: ErrorResponseDto,
-    status: HttpStatus.UNAUTHORIZED,
-  })
-  @ApiInternalServerErrorResponse({
-    type: ErrorResponseDto,
-    status: HttpStatus.INTERNAL_SERVER_ERROR,
   })
   async create(
     @User() user: jwtUserT,
@@ -77,6 +85,34 @@ export class SprintController {
       httpStatus: HttpStatus.CREATED,
       message: '스프린트가 성공적으로 생성되었습니다.',
       item: createdSprint,
+    };
+  }
+
+  @Get('/list')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: '유저의 레포지토리 리스트를 조회한다.' })
+  @ApiOkResponse({
+    type: OutputFindSprintsDto,
+    status: HttpStatus.OK,
+  })
+  async find(
+    @User() user: jwtUserT,
+    @Query() query: PagingRequestDto,
+  ): Promise<OutputFindSprintsDto> {
+    const { page, limit } = query;
+    const { items, totalCount } = await this.sprintService.find({
+      id: user.id,
+      page,
+      limit,
+    });
+
+    return {
+      httpStatus: HttpStatus.OK,
+      message: `${page}p 스프린트 리스트를 성공적으로 조회했습니다.`,
+      currentPage: page,
+      limit,
+      totalCount,
+      items,
     };
   }
 }
--- src/sprint/sprint.service.ts
+++ src/sprint/sprint.service.ts
@@ -1,10 +1,14 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SprintEntity } from './entity/sprint.entity';
 import { Repository } from 'typeorm';
 import { InputCreateSprintDto } from './dto/create-sprint.dto';
-import { ServiceResultDto } from 'src/common/common.dto';
+import {
+  ServicePagingResultDto,
+  ServiceResultDto,
+} from 'src/common/common.dto';
 import { RepoEntity } from 'src/repo/entity/repo.entity';
+import { InputFindSprintsDto } from './dto/find-sprint.dto';
 
 @Injectable()
 export class SprintService {
@@ -28,5 +32,28 @@ export class SprintService {
     Logger.log(`생성 완료`);
 
     return { item: savedSprint };
+  }
+
+  async find(
+    input: InputFindSprintsDto,
+  ): Promise<ServicePagingResultDto<SprintEntity[]>> {
+    const { id: userId, page, limit } = input;
+
+    const queryBuilder = this.sprintRepository
+      .createQueryBuilder('sprint')
+      .where('userId = :userId', { userId })
+      .offset((page - 1) * limit)
+      .limit(limit);
+
+    const [sprints, totalCount] = await queryBuilder.getManyAndCount();
+
+    if (!sprints.length) {
+      throw new NotFoundException(`${page}p에 발견된 스프린트가 없습니다.`);
+    }
+
+    return {
+      items: sprints,
+      totalCount,
+    };
   }
 }
